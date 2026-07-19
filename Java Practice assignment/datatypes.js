da70d50098cf2c8538fc78b24f6d5c09730b5c2d
Akshay Reddy <akshayreddy06e@gmail.com>
2026-07-19 15:36:08 +0530

    Updated Java Practices

---
+++ Java Practice assignment/datatypes.js
@@ -0,0 +1,39 @@
+let name = "Akshay";
+let age = 21;
+let isStudent = true;
+let salary = null;
+let city;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof salary);
+console.log(typeof city);
+
+let a = 20;
+let b = 10;
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Modulus:", a % b);
+
+let x = true;
+let y = false;
+
+console.log(x && y);
+console.log(x || y);
+console.log(!x);
+
+let marks = 75;
+
+if (marks >= 90) {
+    console.log("Grade A");
+} else if (marks >= 75) {
+    console.log("Grade B");
+} else if (marks >= 50) {
+    console.log("Grade C");
+} else {
+    console.log("Fail");
+}
