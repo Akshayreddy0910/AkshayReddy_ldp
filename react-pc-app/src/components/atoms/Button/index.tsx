12de99e73f5a1edd817d0fa84afb27dce9b257ae
Akshayreddy0910 <akshayreddy06e@gmail.com>
2026-08-09 21:47:02 +0530

    Enhance Button component with optional 'sx' prop
    Added optional 'sx' prop for custom styling to Button component.

--- react-pc-app/src/components/atoms/Button/index.tsx
+++ react-pc-app/src/components/atoms/Button/index.tsx
@@ -1,13 +1,15 @@
 import MuiButton from "@mui/material/Button";
+import type { SxProps, Theme } from "@mui/material/styles";
 
 interface ButtonProps {
   label: string;
   onClick?: () => void;
+  sx?: SxProps<Theme>;
 }
 
-const Button = ({ label, onClick }: ButtonProps) => {
+const Button = ({ label, onClick, sx }: ButtonProps) => {
   return (
-    <MuiButton variant="contained" color="primary" onClick={onClick}>
+    <MuiButton onClick={onClick} sx={sx}>
       {label}
     </MuiButton>
   );
